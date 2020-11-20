903db3dac74f61f2fb53e88f411a6b1552c83ba2
apinna1 <apinna.elis@gmail.com>
2020-11-20 15:45:24 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,78 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularEditorModule } from '@kolkov/angular-editor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutes } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxStarsModule } from 'ngx-stars';
 import { PageHomeComponent } from './pages/page-home/page-home.component';
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
+import { DelegateServiceService } from './services/delegate-service.service';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { NavBarComponent } from './navs/nav-bar/nav-bar.component';
+import { SideBarComponent } from './navs/side-bar/side-bar.component';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  observer: true,
+  direction: 'horizontal',
+  threshold: 50,
+  spaceBetween: 5,
+  slidesPerView: 1,
+  centeredSlides: true
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageHomeComponent
+    PageHomeComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
+    RouterModule.forRoot(AppRoutes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      AngularEditorModule,
+      FormsModule,
+      HttpClientModule,
+      NgbModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      NgxStarsModule,
+      MatSidenavModule,
+      MatProgressSpinnerModule,
+      MatButtonModule,
+      MatIconModule,
+      MatSnackBarModule,
+      MatPaginatorModule,
+      MatDividerModule,
+      MatExpansionModule,
+      SwiperModule,
+      BrowserModule,
+      FlexLayoutModule
+    
+    
   ],
-  providers: [],
+  providers: [DelegateServiceService,
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
