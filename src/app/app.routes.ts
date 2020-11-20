ee9465f0eb324b9b35b72581fcc8504230f51f00
apinna1 <apinna.elis@gmail.com>
2020-11-20 14:44:00 +0100

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+ 
+export const AppRoutes: Routes = [
+    { path: '', component: PageHomeComponent }
+  ];
