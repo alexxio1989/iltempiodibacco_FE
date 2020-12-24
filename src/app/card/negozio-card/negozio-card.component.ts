6e31b409980f566c687e7072f1657aee4704f282
Alessio Pinna <alessiopinna@MacBook-Air-di-Alessio.local>
2020-12-24 17:23:40 +0100

    first commit

--- src/app/card/negozio-card/negozio-card.component.ts
+++ src/app/card/negozio-card/negozio-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { Dominio } from 'src/app/model/Dominio';
 import { Negozio } from 'src/app/model/Negozio';
 import { Prodotto } from 'src/app/model/Prodotto';
 
@@ -12,13 +14,32 @@ export class NegozioCardComponent implements OnInit {
   @Input() negozio: Negozio;
 
   pageIndex: number = 0;
-  pageSize: number = 1;
+  pageSize: number = 0;
   lowValue: number = 0;
-  highValue: number = 1;
+  highValue: number = 0;
 
-  constructor() { }
+  tipoSelected: Dominio;
 
-  ngOnInit(): void { }
+  constructor(private deviceService: DeviceDetectorService) { }
+
+  ngOnInit(): void {
+
+    const isMobile = this.deviceService.isMobile();
+
+    if(isMobile){
+
+      this.pageSize = 1;
+      this.highValue = 1;
+    } else {
+      this.pageSize = 3;
+      this.highValue = 3;
+    }
+    
+    if(this.negozio !== undefined && this.negozio.magazino.tipiAssociati.length > 0){
+      this.tipoSelected = this.negozio.magazino.tipiAssociati[0];
+    }
+
+  }
 
   getPaginatorData(event){
     console.log(event);
